591e917fe1a28b26928283690dea9e15e407895f
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 01:33:22 +0300

    Fix todos status logic and remove sort

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,14 +1,24 @@
 "use client"
 
-import moment from "moment"
+import { Button, Separator } from "@radix-ui/themes"
 import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 
 import { ITodo } from "@/interfaces"
-import { DATE_FORMAT_DAY_TIME } from "@/utils/date"
+
+import Todo from "../Todo/Todo"
+
+enum STATUS {
+  ALL = "All",
+  Active = "Active",
+  Completed = "Completed",
+}
 
 const Todos = () => {
   const { data: session, status } = useSession()
+
+  const [filteredTodos, setFilteredTodos] = useState<ITodo[] | []>([])
+  const [filter, setFilter] = useState<STATUS>(STATUS.ALL)
   const [todos, setTodos] = useState<ITodo[] | []>([])
 
   useEffect(() => {
@@ -17,15 +27,22 @@ const Todos = () => {
     }
   }, [session])
 
+  useEffect(() => {
+    if (filter === STATUS.Completed) {
+      setFilteredTodos(todos.filter((todo) => todo.completed))
+    } else if (filter === STATUS.Active) {
+      setFilteredTodos(todos.filter((todo) => !todo.completed))
+    } else {
+      setFilteredTodos(todos)
+    }
+  }, [filter, todos])
+
   const fetchTodos = async () => {
     try {
       const res = await fetch("/api/todos")
       if (!res.ok) throw new Error("Failed to fetch todos")
       const data = await res.json()
-      const sortedTodos = data.sort(
-        (a: ITodo, b: ITodo) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
-      )
-      setTodos(sortedTodos)
+      setTodos(data)
     } catch (err) {
       console.log((err as Error).message)
     }
@@ -47,12 +64,23 @@ const Todos = () => {
   }
 
   const updateTodo = async (id: string, updatedData: any) => {
-    await fetch(`/api/todos/${id}`, {
-      body: JSON.stringify(updatedData),
-      headers: { "Content-Type": "application/json" },
-      method: "PATCH",
-    })
-    fetchTodos()
+    try {
+      const res = await fetch(`/api/todos/${id}`, {
+        body: JSON.stringify(updatedData),
+        headers: { "Content-Type": "application/json" },
+        method: "PATCH",
+      })
+
+      if (!res.ok) {
+        throw new Error("Failed to update todo")
+      }
+
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? { ...todo, ...updatedData } : todo)),
+      )
+    } catch (error) {
+      console.error("Error updating todo:", error)
+    }
   }
 
   const deleteTodo = async (id: string) => {
@@ -75,32 +103,45 @@ const Todos = () => {
   const isLoading = status === "loading"
 
   return (
-    <div>
-      <h1>Your Todos</h1>
+    <div className="p-4 min-h-[80vh] mb-20">
+      <h1 className="text-[30px] font-bold mb-4 text-center">Your Todos</h1>
+      <div className="flex justify-between mb-4 w-[500px]">
+        <Button
+          className="bg-[#000000] text-white px-4 py-2 rounded-md hover:bg-[#3c3b3b] transition-colors focus:outline-none focus:ring-2 focus:ring-red-500"
+          onClick={() => setFilter(STATUS.ALL)}
+        >
+          {STATUS.ALL}
+        </Button>
+        <Button
+          className="bg-[#000000] text-white px-4 py-2 rounded-md hover:bg-[#3c3b3b] transition-colors focus:outline-none focus:ring-2 focus:ring-red-500"
+          onClick={() => setFilter(STATUS.Completed)}
+        >
+          {STATUS.Completed}
+        </Button>
+        <Button
+          className="bg-[#000000] text-white px-4 py-2 rounded-md hover:bg-[#3c3b3b] transition-colors focus:outline-none focus:ring-2 focus:ring-red-500"
+          onClick={() => setFilter(STATUS.Active)}
+        >
+          {STATUS.Active}
+        </Button>
+      </div>
+      <Separator className="my-4" />
       {!isLoading ? (
-        <ul>
-          {todos.length ? (
-            todos.map((todo) => (
-              <li key={todo.id}>
-                {todo.body} {todo.completed.toString()}
-                {moment(todo?.createdAt).format(DATE_FORMAT_DAY_TIME)}
-                {moment(todo?.updatedAt).format(DATE_FORMAT_DAY_TIME)}
-                <button className="border" onClick={() => updateTodo(todo.id, { completed: true })}>
-                  update
-                </button>
-                <button className="border" onClick={() => deleteTodo(todo.id)}>
-                  Delete
-                </button>
-              </li>
+        <ul className="space-y-2">
+          {filteredTodos.length ? (
+            filteredTodos.map((todo) => (
+              <Todo deleteTodo={deleteTodo} key={todo.id} todo={todo} updateTodo={updateTodo} />
             ))
           ) : (
-            <p>no todos</p>
+            <p>No todos</p>
           )}
         </ul>
       ) : (
-        <p>loading</p>
+        <p>Loading...</p>
       )}
-      <button onClick={() => addTodo("New Todo")}>Add Todo</button>
+      <Button className="mt-4" onClick={() => addTodo("New Todo")}>
+        Add Todo
+      </Button>
     </div>
   )
 }
