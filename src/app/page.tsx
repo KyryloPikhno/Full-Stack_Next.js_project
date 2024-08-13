5d9e19bbed0faa6c33626f048d0881c1bb06c43b
KyryloPikhno <Mr.Good@i.ua>
2024-08-13 18:26:07 +0300

    Fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+
 import "@radix-ui/themes/styles.css"
+
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span>page</span>
+    <main className="flex flex-col items-center">
+      <p>Have account?</p>
+      <Link className="border mb-5" href="/auth/login">
+        Login
+      </Link>
+      <p>Don't have account </p>
+      <Link className="border" href="/auth/register">
+        Register
+      </Link>
     </main>
   )
 }
