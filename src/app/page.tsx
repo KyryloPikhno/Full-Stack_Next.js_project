6e6f8707ea2a32e4a889d15e47478180ba81bd64
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 00:34:13 +0300

    Fix Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 "use client"
 
-import "@radix-ui/themes/styles.css"
-
-import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center">
-      <p>Have account?</p>
-      <Link className="border mb-5" href="/auth/login">
-        Login
-      </Link>
-      <p>Don't have account </p>
-      <Link className="border" href="/auth/register">
-        Register
-      </Link>
-    </main>
-  )
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push("/auth/register")
+  }, [router])
+
+  return <div>Redirecting...</div>
 }
