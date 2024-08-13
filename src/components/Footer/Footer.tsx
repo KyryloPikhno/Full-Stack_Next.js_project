765183147e588feaef025a01d59127dcc11db8b6
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 02:42:46 +0300

    Fix spelling mistake

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="h-60px mb-5">
-      <p className="text-[12px] text-[#000000]">The test task was created by Kirill Pikhgo</p>
+      <p className="text-[12px] text-[#000000]">The test task was created by Kyrylo Pikhno</p>
     </div>
   )
 }
