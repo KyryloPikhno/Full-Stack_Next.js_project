7ef3889e564291d26d3362f7eeab2f3ce41f30ac
KyryloPikhno <Mr.Good@i.ua>
2024-08-13 23:24:40 +0300

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,7 @@
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className="h-60px mb-5">
+      <p className="text-[12px] text-[#000000]">The test task was created by Kirill Pikhgo</p>
+    </div>
+  )
 }
