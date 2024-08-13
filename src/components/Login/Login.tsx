f69f73decc5afa624cbe02f895289a00b4afe0d1
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 02:12:51 +0300

    Connect inputField to login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,24 +3,28 @@
 import { yupResolver } from "@hookform/resolvers/yup"
 import { useRouter } from "next/navigation"
 import { signIn } from "next-auth/react"
-import { useForm } from "react-hook-form"
+import { FormProvider, useForm } from "react-hook-form"
 
 import { ILogin } from "@/interfaces"
 import { loginSchema } from "@/validation"
 
+import { InputField } from "../InputField/InputField"
+
 const Login = () => {
   const router = useRouter()
 
+  const methods = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(loginSchema),
+  })
+
   const {
-    register,
     setError,
     clearErrors,
     handleSubmit,
     formState: { errors },
-  } = useForm({
-    mode: "onSubmit",
-    resolver: yupResolver(loginSchema),
-  })
+  } = methods
+
   const onSubmit = async (data: ILogin) => {
     try {
       const res = await signIn("credentials", {
@@ -43,19 +47,21 @@ const Login = () => {
   }
 
   return (
-    <form
-      className="flex flex-col"
-      onChange={() => clearErrors()}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      {errors.root?.message && <div className="error-message">{errors.root?.message}</div>}
-      <input {...register("email")} placeholder="Email" required />
-      {errors.email?.message && <div className="error-message">{errors.email?.message}</div>}
+    <FormProvider {...methods}>
+      <form
+        className="flex flex-col gap-4"
+        onChange={() => clearErrors()}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <InputField name="email" placeholder="Email" />
+        <InputField name="password" placeholder="Password" type="password" />
 
-      <input {...register("password")} placeholder="Password" required type="password" />
-      {errors.password?.message && <div className="error-message">{errors.password?.message}</div>}
-      <button type="submit">Login</button>
-    </form>
+        <div className="relative">
+          <button type="submit">Login</button>
+          {errors.root?.message && <div className="error-message">{errors.root?.message}</div>}
+        </div>
+      </form>
+    </FormProvider>
   )
 }
 
