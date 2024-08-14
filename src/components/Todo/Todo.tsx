5fdb63dbf8d392bdf01dd206b69a58c20c753f1e
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 20:22:26 +0300

    Fix edit logic

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -2,67 +2,76 @@
 
 import { Button, Checkbox } from "@radix-ui/themes"
 import moment from "moment"
-import { FC, useRef, useState } from "react"
+import { FC, useEffect, useRef, useState } from "react"
 
 import { ITodoProps } from "@/interfaces"
 import { DATE_FORMAT_DAY_TIME } from "@/utils/date"
 
 import DeleteIcon from "../Icons/DeleteIcon"
+import { Textarea } from "../ui/textarea"
 
 const Todo: FC<ITodoProps> = ({ todo, updateTodo, deleteTodo }) => {
   const [isEditing, setIsEditing] = useState(false)
-  const [editValue, setEditValue] = useState(todo.body)
-  const inputRef = useRef<HTMLInputElement>(null)
+  const [value, setValue] = useState(todo.body)
 
-  const handleDoubleClick = () => {
-    setIsEditing(true)
-    console.log("lol")
-  }
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
 
-  const handleBlur = () => {
-    setIsEditing(false)
-    if (editValue !== todo.body) {
-      updateTodo(todo.id, { body: editValue })
+  useEffect(() => {
+    if (isEditing && textareaRef.current) {
+      textareaRef.current.focus()
+      const length = textareaRef.current.value.length
+      textareaRef.current.setSelectionRange(length, length)
     }
-  }
+  }, [isEditing])
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEditValue(event.target.value)
+  const handleBlur = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+    console.log("Textarea lost focus", e.target.value)
+    updateTodo(todo.id, { body: e.target.value })
+    setIsEditing(false)
   }
 
   return (
-    <div className="p-[10px] border w-[500px] rounded shadow" key={todo.id}>
-      <div className="flex gap-4 justify-between items-center">
-        <div className="flex items-center gap-2">
-          <div>
-            <Checkbox
-              checked={todo.completed}
-              className="w-6 h-6 border-[2px] flex justify-center items-center border-[#000000] rounded-full"
-              onCheckedChange={() => updateTodo(todo.id, { completed: !todo.completed })}
-            />
+    <>
+      {isEditing ? (
+        <Textarea
+          className="text-[18px]"
+          onBlur={handleBlur}
+          onChange={(e) => setValue(e.target.value)}
+          ref={textareaRef}
+          value={value}
+        />
+      ) : (
+        <div className="px-2 border w-[500px] rounded shadow" key={todo.id}>
+          <div className="flex gap-4 justify-between items-center">
+            <div className="flex items-center gap-2">
+              <div>
+                <Checkbox
+                  checked={todo.completed}
+                  className="w-6 h-6 border-[2px] flex justify-center items-center border-[#000000] rounded-full"
+                  onCheckedChange={() => updateTodo(todo.id, { completed: !todo.completed })}
+                />
+              </div>
+              <div className="py-2 cursor-pointer" onDoubleClick={() => setIsEditing(true)}>
+                <p className="text-lg w-[400px] leading-[23px] break-words">{todo.body}</p>
+                <div className="flex gap-4 mt-1">
+                  <p className="text-[10px] text-gray-500">
+                    Created: {moment(todo?.createdAt).format(DATE_FORMAT_DAY_TIME)}
+                  </p>
+                  <p className="text-[10px] text-gray-500">
+                    Updated: {moment(todo?.updatedAt).format(DATE_FORMAT_DAY_TIME)}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center mr-[-3px]">
+              <Button onClick={() => deleteTodo(todo.id)} type="button">
+                <DeleteIcon />
+              </Button>
+            </div>
           </div>
-          <p
-            className="text-lg min-w-[300px] leading-[23px] border"
-            onDoubleClick={handleDoubleClick}
-          >
-            {todo.body}
-          </p>
         </div>
-        <div className="flex items-center">
-          <Button onClick={() => deleteTodo(todo.id)} type="button">
-            <DeleteIcon />
-          </Button>
-        </div>
-      </div>
-      <div className="flex gap-4 mt-1">
-        <p className="text-[10px] text-gray-500">
-          Created: {moment(todo?.createdAt).format(DATE_FORMAT_DAY_TIME)}
-        </p>
-        <p className="text-[10px] text-gray-500">
-          Updated: {moment(todo?.updatedAt).format(DATE_FORMAT_DAY_TIME)}
-        </p>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
