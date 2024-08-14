673557e0b1b61344395d77fcc0194cd92e09bd87
KyryloPikhno <Mr.Good@i.ua>
2024-08-14 21:47:29 +0300

    Fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 export const Footer = () => {
   return (
-    <div className="h-60px mb-5">
-      <p className="text-[12px] text-[#000000]">The test task was created by Kyrylo Pikhno</p>
+    <div className="h-60px mb-5 text-center">
+      <p className="text-[12px] text-[#000000] animate-pulse">Double-click on a todo to edit it.</p>
+      <p className="text-[12px] text-[#000000]">The test task was created by Kyrylo Pikhno.</p>
     </div>
   )
 }
