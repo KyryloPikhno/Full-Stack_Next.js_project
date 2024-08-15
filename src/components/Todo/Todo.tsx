e350d01f8ed031dbec81e2b41f55a95859f119db
KyryloPikhno <Mr.Good@i.ua>
2024-08-15 14:47:37 +0300

    Add handleKeyDown

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -29,17 +29,33 @@ const Todo: FC<ITodoProps> = ({ todo, updateTodo, deleteTodo }) => {
     setIsEditing(false)
   }
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Escape") {
+      setIsEditing(false)
+    }
+  }
+
+  const onCheckedChange = () => {
+    updateTodo(todo.id, { completed: !todo.completed })
+  }
+
   return (
     <>
       {isEditing ? (
-        <Textarea
-          className="text-[18px]"
-          onBlur={handleBlur}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Enter changes"
-          ref={textareaRef}
-          value={value}
-        />
+        <div className="relative">
+          <Textarea
+            className="text-[18px]"
+            onBlur={handleBlur}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Enter changes"
+            ref={textareaRef}
+            value={value}
+          />
+          <div className="absolute top-[-4px] right-1 text-[9px] text-gray-600 mt-2">
+            Hit Esc to drop it!
+          </div>
+        </div>
       ) : (
         <div className="px-2 border sm:w-[500px] rounded shadow group" key={todo.id}>
           <div className="flex gap-4 justify-between items-center">
@@ -48,11 +64,15 @@ const Todo: FC<ITodoProps> = ({ todo, updateTodo, deleteTodo }) => {
                 <Checkbox
                   checked={todo.completed}
                   className="w-6 h-6 border-[2px] flex justify-center items-center border-[#000000] rounded-full"
-                  onCheckedChange={() => updateTodo(todo.id, { completed: !todo.completed })}
+                  onCheckedChange={onCheckedChange}
                 />
               </div>
               <div className="py-2 cursor-pointer" onDoubleClick={() => setIsEditing(true)}>
-                <p className="text-lg sm:w-[400px] leading-[23px] break-words">{todo.body}</p>
+                <p
+                  className={`text-lg sm:w-[400px] leading-[23px] break-words ${todo.completed ? "line-through" : ""}`}
+                >
+                  {todo.body}
+                </p>
                 <div className="flex gap-4 mt-1">
                   <p className="text-[10px] text-gray-500">
                     Created: {moment(todo?.createdAt).format(DATE_FORMAT_DAY_TIME)}
